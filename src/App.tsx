969900ffb338d4f7b15e074fa85891cb308519f6
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-07-26 17:44:48 +0700

    feat: added zod, react hook form, react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
     <div className="bg-red-400">
       <h1>Welcome to My App</h1>
       <p>This is a simple React application.</p>
+      <Button>abc</Button>
     </div>
   );
 }
