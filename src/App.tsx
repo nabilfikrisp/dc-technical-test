79988f0af4406e5f0eecee9677054f9b5bb735e4
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-07-26 17:35:28 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="bg-red-400">
+      <h1>Welcome to My App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
